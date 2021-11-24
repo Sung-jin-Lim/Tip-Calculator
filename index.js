022daa57cc60d5ec60a3e89d65b41886824adc1e
suspiciouscouch <sungjinlim.real@gmail.com>
2021-11-24 21:24:17 +0700

    everything execept reset button and ios media query

--- index.js
+++ index.js
@@ -8,12 +8,6 @@ let totalAmount = document.querySelector("#total");
 let errorMessage = document.querySelector(".invis");
 let customTip = document.querySelector(".custom");
 
-// button focus event listener
-
-// tip[0].addEventListener("click", function () {
-//   tip[0].classList.add("focus");
-// });
-
 tip.forEach;
 (function (aTip) {
   aTip.addEventListener("input", calculateTip);
@@ -25,7 +19,7 @@ customTip.addEventListener("change", calculateTip);
 
 // add class 2 one button and remvoe it from all other buttons\
 
-// buttons
+// when a button or input is clicked it recieves class and removes same class from other buttons
 
 for (let i = 0; i < 6; i++) {
   tip[i].addEventListener("click", function () {
@@ -46,7 +40,7 @@ function calculateTip() {
   let billAmount = document.querySelector("#bill").value;
   let peopleCount = document.querySelector("#peopleCount").value;
   let tipAmount = document.querySelector("#tipAmount");
-  let totalAmount = document.querySelector("#total").innerHTML;
+  let totalAmount = document.querySelector("#total");
   let customTip = document.querySelector(".custom");
   // error message
   if (billAmount > 0) {
@@ -55,21 +49,40 @@ function calculateTip() {
     } else {
       errorMessage.classList.add("invis");
       if (customTip.classList.contains("focus") && customTip.value > 0) {
+        // tip amount
         let tipPercent = customTip.value;
         tipAmount.innerHTML = (
           (billAmount * (tipPercent / 100)) /
           peopleCount
         ).toFixed(2);
         console.log(tipAmount);
+
+        // total amount
+        totalAmount.innerHTML = Math.round(
+          +billAmount / +peopleCount +
+            (+billAmount * +(tipPercent / 100)) / +peopleCount
+        );
       } else {
+        // tip amount
         let tipPercent = parseFloat(document.querySelector(".focus").innerHTML);
         tipAmount.innerHTML =
           "$" + ((billAmount * (tipPercent / 100)) / peopleCount).toFixed(2);
         console.log(tipAmount);
 
-        totalAmount.innerHTML =
-          "$" + billAmount + billAmount * (tipPercent / 100) * peopleCount;
+        totalAmount.innerHTML = Math.round(
+          +billAmount / +peopleCount +
+            (+billAmount * +(tipPercent / 100)) / +peopleCount
+        );
+        // "$" + ((billAmount * (tipPercent / 100)) / peopleCount).toFixed(2);
       }
     }
   }
+
+  if (totalAmount.innerHTML > 0) {
+    document.querySelector(".reset").classList.remove("inactive");
+  }
 }
+
+document.querySelector(".reset").addEventListener(click, function () {
+  reload();
+});
